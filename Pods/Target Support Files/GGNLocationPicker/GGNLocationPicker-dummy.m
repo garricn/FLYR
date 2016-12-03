f4937bd13fee162f1733909f832393bbdf279a1b
Garric Nahapetian <garricn@icloud.com>
2016-12-03 15:04:58 -0800

    Ignore Pods

--- Pods/Target Support Files/GGNLocationPicker/GGNLocationPicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GGNLocationPicker : NSObject
-@end
-@implementation PodsDummy_GGNLocationPicker
-@end
--- Pods/Target Support Files/GGNObservable/GGNObservable-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double GGNObservableVersionNumber;
-FOUNDATION_EXPORT const unsigned char GGNObservableVersionString[];
-
